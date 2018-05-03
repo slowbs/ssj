cb577c841227e61afc5e1ae174690477e89abcc8
slowbs <poison_word@hotmail.com>
2018-05-03 15:34:33 +0700

    modal form not work
    modal form ปุ่มใน footer ไม่เวิร์ค

--- modal_ajax.php
+++ modal_ajax.php
@@ -3,8 +3,26 @@
   <h4 id="modalTitle" class="modal-title"></h4>
 </div>
 <div class="modal-body">
-  <?php echo "ID: ".$_GET['id'];?>
+<?php $id = isset($_GET['id']) ? $_GET['id'] : ''; 
+     //echo "ID: $id "?>
+         <div class="container">
+            <h1>Insert Form</h1>
+            <form action="insert.php" method="POST"> 
+                    <div class="form-group">
+                      <label for="name">ชื่อ - สกุล</label>
+                      <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="ชื่อ - สกุล" name="name">
+                      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                      <label for="price">ราคา</label>
+                      <input type="text" class="form-control" id="price" placeholder="ราคา" name="price">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+    </div>
+
 </div>
 <div class="modal-footer">
+<button type="submit" class="btn btn-primary">Submit</button>
   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 </div>
